12db3a161257a09e64631e2fbf80ab88f6ca631b
Peyman <peyman.osatian@gmail.com>
2025-05-04 14:10:15 +0200

    login view

---
+++ OnlineGrocerySwiftUI/ViewModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  OnlineGrocerySwiftUI
+//
+//  Created by Peyman Osatian on 2025-05-03.
+//
+
+import SwiftUI
+
+struct MainViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewModel()
+}
