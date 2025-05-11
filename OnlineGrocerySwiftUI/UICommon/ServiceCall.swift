1c57c662f54835ebbdb5cfd32b76b70991668583
Peyman <peyman.osatian@gmail.com>
2025-05-11 18:57:58 +0200

    Signupview start

---
+++ OnlineGrocerySwiftUI/UICommon/ServiceCall.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceCall.swift
+//  OnlineGrocerySwiftUI
+//
+//  Created by Peyman Osatian on 2025-05-10.
+//
+
+import SwiftUI
+
+struct ServiceCall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ServiceCall()
+}
---
+++ OnlineGrocerySwiftUI/View/Login/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  OnlineGrocerySwiftUI
+//
+//  Created by Peyman Osatian on 2025-05-11.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
